e9cfe3edb17568de2b6acedeb46a9c43f6f71a00
Venkata-Vamsi12 <87063565+Venkata-Vamsi12@users.noreply.github.com>
2021-07-09 23:01:42 +0530

    day_2 assignmnets

--- day_2.js
+++ day_2.js
@@ -19,3 +19,21 @@ for(i=1; i<=100; i++){
     console.log(s)
   }
 }
+
+// second assignment
+
+s = "madam is walking with her dad and mom"
+s = s+ " "
+word = ""
+for(k of s){
+  if(k != " "){
+    word = word + k
+  }
+  else{
+    rev = word.split("").reverse().join("")
+    if(rev == word){
+      console.log(word)
+    }
+    word = ""
+  }
+}
